3d0b8122db0014c37c10e8f17273a2104bfaf812
Matt Nardoni <nardoni59@gmail.com>
2018-04-20 17:00:46 -0700

    Add time.js

--- src/earth.js
+++ src/earth.js
@@ -1,3 +1,8 @@
-export function EarthAge(years) {
-  this.years = years;
+class Age {
+  constructor (years) {
+    const earthSec = 31536000;
+    this.years = years * earthSec;
+  }
 }
+
+export {Age};
--- src/planets.js
+++ src/planets.js
@@ -1,7 +1,13 @@
 class Planet {
   constructor (years) {
     this.merc = years / (0.24);
+    this.venus = years / (0.62);
+    this.mars = years / (1.88);
+    this.jupiter = years / (11.86);
   }
+
+  
+
 }
 
 export {Planet};
