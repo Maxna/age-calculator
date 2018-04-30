c9369d0e0b08238b1103f29dbb4d90bdb1ec5f8d
Matt Nardoni <mattnardoni@MattNardoni.local>
2018-04-30 01:01:49 -0700

    Add input variable for planet methods

--- src/planets.js
+++ src/planets.js
@@ -1,13 +1,27 @@
 class Planet {
   constructor (years) {
-    this.merc = years / (0.24);
-    this.venus = years / (0.62);
-    this.mars = years / (1.88);
-    this.jupiter = years / (11.86);
+    this.years = years;
   }
 
-  
+  mercury() {
+    let mercAge = this.years * (0.24);
+    return mercAge;
+  }
 
+  venus() {
+    let venusAge = this.venus * (0.62);
+    return venusAge;
+  }
+
+  mars() {
+    let marsAge = this.mars * (1.88);
+    return marsAge;
+  }
+
+  jupiter() {
+    let jupiterAge = this.jupiter *(11.86);
+    return jupiterAge;
+  }
 }
 
 export {Planet};
